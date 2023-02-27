51b23bf7c0b73b881dd64ae8a2802ac3751407e1
DenizIsikli <s215818@dtu.dk>
2023-02-27 23:03:04 +0100

    Initial commit

---
+++ src/Assigment1.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.util.*;
+
+public class Assigment1 {
+    public static void priceHunter(int[] A) {
+        Arrays.sort(A);
+        int discout = 0;
+        int n = A.length;
+
+        System.out.println(n);
+
+        for(int i = n -1; i >= 2; i-=3) {
+            //Udregner
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] A = new int[n];
+
+
+        for (int i = 0; i < n; i++) {
+            A[i] = scanner.nextInt();
+        }
+
+        priceHunter(A);
+    }
+}

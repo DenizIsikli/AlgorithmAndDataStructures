0c3821f3400855e9c10b075a540a79680338659c
DenizIsikli <s215818@dtu.dk>
2023-02-28 21:35:08 +0100

    Minor changes, the solutions works.

--- src/Assigment1.java
+++ src/Assigment1.java
@@ -3,25 +3,23 @@ import java.util.*;
 public class Assigment1 {
     public static void priceHunter(int[] A) {
         Arrays.sort(A);
-        int discout = 0;
-        int n = A.length;
+        int discount = 0;
 
-        System.out.println(n);
-
-        for(int i = n -1; i >= 2; i-=3) {
-            //Udregner
+        // I move 3 indices backwards at a time from the end of the list,
+        // so I start with the biggest numbers in A, since I sorted the array
+        for(int i = A.length - 1; i >= 2; i-=3) {
+            // I add the last element of the 3 integers,
+            // since that is the cheapest out of the 3
+            discount += A[i-2];
         }
-
-
+        System.out.println(discount);
     }
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
         int[] A = new int[n];
-
 
         for (int i = 0; i < n; i++) {
             A[i] = scanner.nextInt();

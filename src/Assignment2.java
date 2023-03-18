8a0bfc96bfbb6411d7d9ca4782e3ba68ceb35704
DenizIsikli <s215818@dtu.dk>
2023-03-18 22:57:37 +0100

    Initial commit

---
+++ src/Assignment2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Assignment2 {
+    public static void pathFinder() {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n_buildings = scanner.nextInt();
+        int m_paths = scanner.nextInt();
+        int a_start = scanner.nextInt();
+        int b_end = scanner.nextInt();
+
+
+    }
+}

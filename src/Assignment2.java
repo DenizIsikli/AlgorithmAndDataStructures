28d0e80c5fcd7500f4f4b38a9b8ec3f10b71b41f
DenizIsikli <s215818@dtu.dk>
2023-03-22 19:14:33 +0100

    Final commit

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -4,7 +4,7 @@ public class Assignment2 {
 
     public static boolean bfs(ArrayList<Integer>[] paths, boolean[] visited, int start, int target) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
+        queue.offer(start);
         visited[start] = true;
 
         while (!queue.isEmpty()) {
@@ -13,10 +13,10 @@ public class Assignment2 {
                 return true;
             }
 
-            for (int next : paths[curr]) {
+            for (int next : paths.get(curr)) {
                 if (!visited[next]) {
                     visited[next] = true;
-                    queue.add(next);
+                    queue.offer(next);
                 }
             }
         }
@@ -35,23 +35,23 @@ public class Assignment2 {
         int b = scanner.nextInt();
 
         //list for vertices
-        ArrayList<Integer>[] paths = new ArrayList[n+1];
-        for (int i = 1; i <= n; i++) {
-            paths[i] = new ArrayList<Integer>();
+        List<List<Integer>> paths = new ArrayList<>();
+        for (int i = 0; i < n+1; i++) {
+            paths.add(new ArrayList<>());
         }
 
         //edges
-        for (int i = 1; i <= m; i++) {
+        for (int i = 0; i < m; i++) {
             int u = scanner.nextInt();
             int v = scanner.nextInt();
-            paths[u].add(v);
-            paths[v].add(u);
+            paths.get(u).add(v);
+            paths.get(v).add(u);
         }
 
 
-        //use BFS to search for a path from a to b
+        // use BFS to search for a path from a to b
         boolean[] visited = new boolean[n+1];
-        if(bfs(paths, visited, a, b)) {
+        if (bfs(paths, visited, a, b)) {
             System.out.println("YES");
         } else {
             System.out.println("NO");

077cc57bcef24557bd4df8e5ba754f9f72aeef5b
DenizIsikli <s215818@dtu.dk>
2023-04-11 00:00:47 +0200

    Minor changes

--- src/Week8FastUnionFind.java
+++ src/Week8FastUnionFind.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
-public class Week8FastUnion {
+public class Week8FastUnionFind {
     private final int[] parent;
     private final int[] rank;
 
-    public Week8FastUnion(int n) {
+    public Week8FastUnionFind(int n) {
         parent = new int[n];
         rank = new int[n];
         for(int i = 0; i < n; i++) {
@@ -13,10 +13,14 @@ public class Week8FastUnion {
     }
 
     private int find(int x) {
-        if(parent[x] != x) {
-            parent[x] = find(parent[x]);
+        int root = x;
+        while (parent[root] != root) {
+            int parentOfRoot = parent[root];
+            int grandparentOfRoot = parent[parentOfRoot];
+            parent[root] = grandparentOfRoot;
+            root = grandparentOfRoot;
         }
-        return parent[x];
+        return root;
     }
 
     public void union(int x, int y) {
@@ -36,7 +40,7 @@ public class Week8FastUnion {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        Week8FastUnion uf = new Week8FastUnion(n);
+        Week8FastUnionFind uf = new Week8FastUnionFind(n);
 
         for(int i = 0; i < m; i++) {
             String operation = scanner.next();
